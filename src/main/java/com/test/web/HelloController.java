140b59e696f9774440d308a20cb371a62d6bf883
dasouche1 <shenfeilong@souche.com>
2018-06-14 17:51:32 +0800

    shenfl add init files

---
+++ src/main/java/com/test/web/HelloController.java
@@ -0,0 +1,19 @@
+package com.test.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by shenfl on 2018/5/21
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("msg", "Spring MVC Hello World");
+        model.addAttribute("name", "yuntao");
+        return "test";
+    }
+}
